2047d2cf3c54b3c27aeb4c80f5ec00750fd9f457
batista-neto <jbneto9@gmail.com>
2023-10-07 00:47:51 -0300

    Implementado teste simples para o parseExpression

--- src/test/java/br/ufma/ecp/ParserTest.java
+++ src/test/java/br/ufma/ecp/ParserTest.java
@@ -3,14 +3,9 @@ package br.ufma.ecp;
 import java.nio.charset.StandardCharsets;
 
 import static org.junit.Assert.assertEquals;
-
 import java.io.IOException;
-
 import org.junit.Test;
 
-import br.ufma.ecp.Parser;
-import br.ufma.ecp.token.Token;
-import br.ufma.ecp.token.TokenType;
 
 public class ParserTest extends TestSupport {
 
@@ -71,6 +66,31 @@ public class ParserTest extends TestSupport {
           result = result.replaceAll("\r", ""); // no codigo em linux não tem o retorno de carro
           assertEquals(expectedResult, result);    
   
+    }
+
+    @Test
+    public void testParseExpressionSimple() {
+        var input = "10+20;";
+        var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
+        parser.parseExpression();
+        
+        var expectedResult =  """
+          <expression>
+          <term>
+          <integerConstant> 10 </integerConstant>
+          </term>
+          <symbol> + </symbol>
+          <term>
+          <integerConstant> 20 </integerConstant>
+          </term>
+          </expression>
+          """;
+              
+          var result = parser.XMLOutput();
+          result = result.replaceAll("\r", ""); 
+          expectedResult = expectedResult.replaceAll("  ", "");
+          assertEquals(expectedResult, result);    
+
     }
 
     /* 
