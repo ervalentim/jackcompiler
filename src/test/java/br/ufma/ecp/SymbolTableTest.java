66f7485f9faa0533339733af9d192973e1750f5b
Emanuel Rodrigues Valentim <ervalentim@gmail.com>
2023-10-25 12:56:07 -0300

    Teste da Função Resolve do SymbolTable

--- src/test/java/br/ufma/ecp/SymbolTableTest.java
+++ src/test/java/br/ufma/ecp/SymbolTableTest.java
@@ -15,4 +15,16 @@ public class SymbolTableTest {
         sb.define("var2", "int", Kind.ARG);
         assertEquals(2,sb.varCount(Kind.ARG));
     }
+    @Test
+    public void testResolve () {
+        SymbolTable sb = new SymbolTable();
+        sb.define("var1", "int", Kind.ARG);
+        SymbolTable.Symbol s = sb.resolve("var1");
+        assertEquals("var1",s.name());
+
+        sb.define("var2", "int", Kind.FIELD);
+        s = sb.resolve("var2");
+        assertEquals("var2",s.name());
+
+    }
 }
